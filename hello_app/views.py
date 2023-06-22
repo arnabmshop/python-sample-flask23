1814c26abac7b6272aa21becc2793ac77da55d97
arnabmshop <137401894+arnabmshop@users.noreply.github.com>
2023-06-23 01:42:39 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template,request
-# from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient
 from . import app
 
-# CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=arnabsa;AccountKey=GlWpxAlG70eELtWZaz0FrbYyZqLGApX9tSxNLCSDDSjbdYsgbRMYCL/IlSFRQFf5mVcBKPno7XoZ+AStsx90rA==;EndpointSuffix=core.windows.net'
-# CONTAINER_NAME = 'azureml'
-# blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=arnabsa;AccountKey=GlWpxAlG70eELtWZaz0FrbYyZqLGApX9tSxNLCSDDSjbdYsgbRMYCL/IlSFRQFf5mVcBKPno7XoZ+AStsx90rA==;EndpointSuffix=core.windows.net'
+CONTAINER_NAME = 'azureml'
+blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
 
 @app.route("/")
 def home():
@@ -15,8 +15,8 @@ def home():
 def upload():
     file = request.files['file']
     if file:
-        # blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
-        # blob_client.upload_blob(file)
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
+        blob_client.upload_blob(file)
         return 'File uploaded successfully'
     return 'No file selected'
 
