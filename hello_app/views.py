a96ec724cedfa9c0a4120c85c91b400d7d97efef
arnabmshop <137401894+arnabmshop@users.noreply.github.com>
2023-06-23 00:18:17 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -5,10 +5,6 @@ from . import app
 CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=arnabsa;AccountKey=GlWpxAlG70eELtWZaz0FrbYyZqLGApX9tSxNLCSDDSjbdYsgbRMYCL/IlSFRQFf5mVcBKPno7XoZ+AStsx90rA==;EndpointSuffix=core.windows.net'
 CONTAINER_NAME = 'azureml'
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
@@ -17,24 +13,3 @@ def upload():
         blob_client.upload_blob(file)
         return 'File uploaded successfully'
     return 'No file selected'
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
