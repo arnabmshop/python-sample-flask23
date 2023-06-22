4bb4d32951e00ca1b8c4ff97ce6e813ed94cb54f
arnabmshop <137401894+arnabmshop@users.noreply.github.com>
2023-06-23 00:12:04 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -2,9 +2,21 @@ from datetime import datetime
 from flask import Flask, render_template
 from . import app
 
+CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=arnabsa;AccountKey=GlWpxAlG70eELtWZaz0FrbYyZqLGApX9tSxNLCSDDSjbdYsgbRMYCL/IlSFRQFf5mVcBKPno7XoZ+AStsx90rA==;EndpointSuffix=core.windows.net'
+CONTAINER_NAME = 'azureml'
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
+        blob_client.upload_blob(file)
+        return 'File uploaded successfully'
+    return 'No file selected'
 
 @app.route("/about/")
 def about():
