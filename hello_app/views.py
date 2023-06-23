54e37aa5db0122a8aa8a661d322081363f291ea7
arnabmshop <137401894+arnabmshop@users.noreply.github.com>
2023-06-23 15:54:34 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,76 +1,48 @@
-import os
-import tempfile
-from flask import Flask, render_template, request, send_file
-from PyPDF2 import PdfFileReader
-from docx import Document
+from flask import Flask, render_template, request
 
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import torch
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+model_name = "google/pegasus-xsum"
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
 
-
-def generate_summary(pdf_path):
-    # Load the PDF file
-    with open(pdf_path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        num_pages = pdf.getNumPages()
-
-        # Extract text from each page
-        text = ''
-        for page_num in range(num_pages):
-            page = pdf.getPage(page_num)
-            text += page.extract_text()
-
-        # Generate summary (you can replace this with your own summarization logic)
-        summary = text[:500]  # Example: Take the first 500 characters as a summary
-
-    return summary
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+@app.route('/text-summarization', methods=["POST"])
+def summarize():
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Check if a file is selected for upload
-    if 'file' not in request.files:
-        return 'No file selected'
+    if request.method == "POST":
 
-    file = request.files['file']
+        inputtext = request.form["inputtext_"]
 
-    # Check if a file is uploaded
-    if file.filename == '':
-        return 'No file selected'
+        input_text = "summarize: " + inputtext
 
-    # Save the uploaded file to a temporary location
-    temp_dir = tempfile.mkdtemp()
-    temp_path = os.path.join(temp_dir, file.filename)
-    file.save(temp_path)
+        tokenized_text = tokenizer.encode(input_text, return_tensors='pt', max_length=512).to(device)
+        summary_ = model.generate(tokenized_text, min_length=30, max_length=300)
+        summary = tokenizer.decode(summary_[0], skip_special_tokens=True)
 
-    # Show "file uploaded successfully" message
-    message = 'File uploaded successfully'
+        '''
+            text = <start> i am yash <end>
+            vocab = { i: 1, am : 2, yash: 3, start 4}
 
-    # Process the uploaded file
-    message = 'File is being processed...'
-    summary = generate_summary(temp_path)
+            token = [i, am ,yash]
+            encode = [1 2, 3, 4]
 
-    # Create a Word document with the summary
-    doc = Document()
-    doc.add_paragraph(summary)
-    doc_path = os.path.join(temp_dir, 'summary.docx')
-    doc.save(doc_path)
+            summary_ = [[4, 3,1, 5]]
 
-    # Remove the temporary directory
-    os.remove(temp_path)
-    os.rmdir(temp_dir)
+            summary = yash i
 
-    # Provide the summary document as a download
-    return send_file(doc_path, as_attachment=True)
+        
+        '''
 
+    return render_template("output.html", data = {"summary": summary})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': # It Allows You to Execute Code When the File Runs as a Script
+    app.run()
